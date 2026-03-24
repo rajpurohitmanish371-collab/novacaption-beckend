3832b6fbdcff8d110ad24548b3ba626da37ef908
rajpurohitmanish371-collab <rajpurohitmanish371@gmail.com>
2026-03-24 10:45:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "NovaCaption AI Backend Running 🚀"
-  });
+  res.json({ message: "NovaCaption AI Backend Running 🚀" });
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
